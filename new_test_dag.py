2e4e51fc83b5a0b3170da12cf50ab2b6bce1880c
Dmitriy Redkin <expert.info79@gmail.com>
2021-12-02 22:38:47 +0300

    Delete new_test_dag.py

--- new_test_dag.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from datetime import datetime
-from datetime import timedelta
-
-from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
